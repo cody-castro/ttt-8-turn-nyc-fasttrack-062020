16383a5e66d3c79a69a7150363256c54218398e6
Cody C <codyicastro@gmail.com>
2020-06-24 01:14:22 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,31 @@
+def display_board(arr)
+  puts board
+end
+
+def valid_move
+  X = TRUE
+  O = TRUE
+  if input == FALSE
+    puts "Invalid Move"
+
+
+
+def ask(your_turn)
+  convert(your_turn) in to index
+  if index == TRUE
+    make the move for index
+    puts board
+  else
+    puts "Please enter valid input"
+  end
+end
+
+# ask for input
+# get input
+# convert input to index
+# if index is valid
+#   make the move for index
+#   show the board
+# else
+#   ask for input again until you get a valid input
+# end
